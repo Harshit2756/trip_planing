485d7fcb0f0789583e27838885b4aab9de9e9f80
Harshit Khandelwal <86138193+Harshit2756@users.noreply.github.com>
2025-03-04 02:50:04 +0530

    refactor: simplify main app initialization and update social link URLs

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:device_preview_screenshot/device_preview_screenshot.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -8,55 +7,11 @@ import 'package:trip_planning/firebase_options.dart';
 import 'core/routes/routes.dart';
 import 'core/routes/routes_name.dart';
 import 'core/theme.dart';
-import 'device_preview_button.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-    DevicePreview(
-      backgroundColor: Colors.white,
-      tools: const [
-        CustomPlugin(
-          apkDownloadUrl: "",
-          sourceCodeUrl: "",
-        ),
-        DeviceSection(
-          frameVisibility: true,
-          orientation: false,
-        ),
-        SystemSection(
-          locale: false,
-          theme: true,
-        ),
-        DevicePreviewScreenshot(),
-        SettingsSection(
-          backgroundTheme: true,
-          toolsTheme: true,
-        ),
-      ],
-      devices: [
-        Devices.android.samsungGalaxyA50,
-        Devices.android.samsungGalaxyNote20,
-        Devices.android.samsungGalaxyS20,
-        Devices.android.samsungGalaxyNote20Ultra,
-        Devices.android.onePlus8Pro,
-        Devices.android.sonyXperia1II,
-        Devices.ios.iPhoneSE,
-        Devices.ios.iPhone12,
-        Devices.ios.iPhone12Mini,
-        Devices.ios.iPhone12ProMax,
-        Devices.ios.iPhone13,
-        Devices.ios.iPhone13ProMax,
-        Devices.ios.iPhone13Mini,
-        Devices.ios.iPhoneSE,
-      ],
-      enabled: true,
-      builder: (context) => const MyApp(),
-    ),
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
